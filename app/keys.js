36dda8e449eb3c2573e1e917a94102a5a5059cef
Mike West <mister270@gmail.com>
2018-06-12 19:23:35 -0700

    initial structure

---
+++ app/keys.js
@@ -0,0 +1,7 @@
+exports.mysql = {
+    MYSQL_HOST: process.env.MYSQL_HOST,
+    MYSQL_USER_ID: process.env.MYSQL_USERID,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+    MYSQL_DB: process.env.MYSQL_DB,
+    MYSQL_PORT: process.env.MYSQL_PORT
+};
