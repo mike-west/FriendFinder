fd8bc314a10f1024d46c3ef2eb0138c110f6cb21
Mike West <mister270@gmail.com>
2018-06-14 20:29:57 -0700

    update .gitignore

--- app/keys.js
+++
@@ -1,7 +0,0 @@
-exports.mysql = {
-    MYSQL_HOST: process.env.MYSQL_HOST,
-    MYSQL_USER_ID: process.env.MYSQL_USERID,
-    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
-    MYSQL_DB: process.env.MYSQL_DB,
-    MYSQL_PORT: process.env.MYSQL_PORT
-};
