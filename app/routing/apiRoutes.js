0c955b88ff618f3e94e893f7466eb09f2ab6b19d
Mike West <mister270@gmail.com>
2018-06-17 21:34:20 -0700

    Add new user only once

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,8 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         var match = req.body;
         var user = req.body;
-        console.log(user);
         var alreadyThere = false;
+        var alreadyAdded = false;
         var userScore = 0;
         var closestDiff = 90;
         var closestMatch = {};
@@ -15,37 +15,36 @@ module.exports = function (app) {
         var userScore = totalScore(user.scores);
 
         friendArr.forEach(friend => {
-            if (user.userName === friend.userName) {
+            if (user.userName.toUpperCase() === friend.userName.toUpperCase()) {
+                console.log('setting alreadyThere to true');
                 alreadyThere = true;
                 return; // use this to leave the function and do the next iteration
+            } else {
+                alreadyThere = false;
             }
+
+            console.log(user.userName + " " + friend.userName + " " + alreadyThere);
+
             friendScore = totalScore(friend.scores);
             // the smallest difference between scores is the best match
             var diff = Math.abs(userScore - friendScore);
-            console.log("user score: " + userScore +
-                " friendScore: " + friendScore +
-                " diff: " + diff + " closests diff: " + closestDiff);
+
             if (diff < closestDiff) {
                 // best match so far
                 closestMatch = friend;
-                console.log("friend");
-                console.log(friend);
                 closestDiff = diff;
             }
 
             // append this person to the array if they aren't already there.
-            if (!alreadyThere) {
-                console.log("pushing");
-                console.log(user);
+            if (alreadyThere === false && alreadyAdded === false) {
                 friendArr.push(user);
+                alreadyAdded = true;
             }
 
         });
         // console.log(closestMatch);
 
         // return the match
-        console.log("returning");
-        console.log(closestMatch);
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify(closestMatch));
     });
@@ -59,10 +58,7 @@ function totalScore(scores) {
     var totalScore = 0;
 
     scores.forEach(score => {
-        // console.log("score: " + score);
         totalScore += parseInt(score);
     });
-    console.log("totalScore");
-    console.log(totalScore);
     return totalScore;
 }
